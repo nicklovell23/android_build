9b67e4522d5c7a2eead632332b054f0cee4059e2
Paul Beeler <pbeeler80@gmail.com>
2015-05-23 15:29:08 -0600

    Disable OpenMP for arm64 until the issues are resolved...
    Change-Id: I1447ca2269ef8c43efe33430c5f3faabb08d8a86
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- core/sabermod/extra.mk
+++ core/sabermod/extra.mk
@@ -73,13 +73,15 @@ ifneq ($(filter arm arm64,$(TARGET_ARCH)),)
       else
         LOCAL_CFLAGS := -fsanitize=leak
       endif
-      ifneq (1,$(words $(filter libwebviewchromium libc_netbsd,$(LOCAL_MODULE))))
-        LOCAL_CFLAGS += -lgomp -lgcc -fopenmp
-        ifdef LOCAL_LDLIBS
-          LOCAL_LDLIBS += -lgomp -lgcc
-        else
-          LOCAL_LDLIBS := -lgomp -lgcc
-        endif
+      ifneq ($(strip $(TARGET_ARCH)),arm64)
+        ifneq (1,$(words $(filter libwebviewchromium libc_netbsd,$(LOCAL_MODULE))))
+          LOCAL_CFLAGS += -lgomp -lgcc -fopenmp
+          ifdef LOCAL_LDLIBS
+            LOCAL_LDLIBS += -lgomp -lgcc
+          else
+            LOCAL_LDLIBS := -lgomp -lgcc
+          endif
+       endif
       endif
     endif
   endif

7461dfbb3d876f5bde98fd145066e35dad35d734
ZION959 <ziontran@gmail.com>
2015-05-27 18:31:28 -0700

    Compile TARGET MODULES with the Snapdragon LLVM Compiler 3.6 for Android instead of LLVM which comes with Android
    Credit all to MusterMaxMueller ( Many Thanks for all the help to have Qclang Optimization intergrated to CMRemix)
    
    https://developer.qualcomm.com/mobile-development/increase-app-performance/snapdragon-llvm-compiler-android
    
    Change-Id: I3889b8f0908be68dcf166223f960a057a46bf798

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -23,11 +23,74 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
   -fno-strict-volatile-bitfields \
   -fno-align-jumps \
   -Wa,--noexecstack \
-  -mfpu=neon-vfpv4
+  -mfpu=neon-vfpv4 \
+  -Wno-unused-local-typedefs \
+  -fpredictive-commoning \
+  -ftree-loop-distribute-patterns \
+  -fvect-cost-model \
+  -ftree-partial-pre \
+  -fipa-cp-clone \
+  -mvectorize-with-neon-quad \
+  -fno-if-conversion
+
+CLANG_QCOM_CONFIG_arm_UNKNOWN_CFLAGS := \
+-fipa-pta \
+-fsection-anchors \
+-ftree-loop-im \
+-ftree-loop-ivcanon \
+-fno-canonical-system-headers \
+-frerun-cse-after-loop \
+-fgcse-las \
+-fgcse-sm \
+-fivopts \
+-frename-registers \
+-ftracer \
+-funsafe-loop-optimizations \
+-funswitch-loops \
+-fweb \
+-fgcse-after-reload \
+-frename-registers \
+-finline-functions \
+-fno-strict-volatile-bitfields \
+-fno-unswitch-loops
 
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
   $(subst -mcpu=cortex-a15,-march=armv7-a,\
+  $(1))))
+endef
+
+#CLANG QCOM
+define subst-clang-qcom-incompatible-arm-ldflags
+  $(subst -mfpu=neon,-mfpu=neon-vfpv4,\
+  $(1))
+endef
+
+define subst-clang-qcom-incompatible-arm-ldflags
+  $(1)
+endef
+
+define subst-clang-qcom-incompatible-arm-flags
+  $(subst -march=armv5te,-mcpu=krait,\
+  $(subst -march=armv5e,-mcpu=krait,\
+  $(subst -march=armv7,-mcpu=krait,\
+  $(subst -march=armv7-a,-mcpu=krait,\
+  $(subst -mcpu=cortex-a15,-mcpu=krait,\
+  $(subst -mfpu=cortex-a8,-mcpu=scorpion,\
+  $(subst -O3,-Ofast -fno-fast-math,\
+  $(subst -Os ,-Os -falign-os ,\
+  $(1)))))))))
+endef
+
+define subst-clang-qcom-O-flags
+  $(subst -O3,-Ofast -fno-fast-math,\
+  $(1))
+endef
+
+define subst-clang-qcom-parallel-flags
+  $(subst -O2,-O2 -fparallel,\
+  $(subst -O3,-O3 -fparallel,\
+  $(subst -Ofast,-Ofast -fparallel,\
   $(1))))
 endef

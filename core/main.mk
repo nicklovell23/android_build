28a733d4f3d7888207cfb82f513dd6eecc456f39
ZION959 <ziontran@gmail.com>
2015-07-10 23:56:43 -0700

    revert: Add a option to disable debugging [1/2]
    Change-Id: I3621af889554224ce8439bdea75b9a323a5374c2
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com> (reverted from commit 7bd3e0269cbc61532555b86b79cc3d662a417826)

--- core/main.mk
+++ core/main.mk
@@ -326,24 +326,17 @@ user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
-  ifneq ($(strip $(FORCE_DISABLE_DEBUGGING)),true)
-    ifeq ($(user_variant),userdebug)
 
-      # Pick up some extra useful tools
-      tags_to_install += debug
+  ifeq ($(user_variant),userdebug)
+    # Pick up some extra useful tools
+    tags_to_install += debug
 
-      # Enable Dalvik lock contention logging for userdebug builds.
-      ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.lockprof.threshold=500
-    else
-      # Disable debugging in plain user builds.
-      enable_target_debugging :=
-    endif
+    # Enable Dalvik lock contention logging for userdebug builds.
+    ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.lockprof.threshold=500
   else
-
-    # Force debugging off.
+    # Disable debugging in plain user builds.
     enable_target_debugging :=
   endif
 
